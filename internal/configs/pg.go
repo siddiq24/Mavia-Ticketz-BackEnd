7794884a80db7c98704d0027ed73ab899680fc2d
siddiq24 <siddiqrm24@gmail.com>
2025-09-08 09:16:36 +0700

    feat: add configuration for connecting to database posgresql

---
+++ internal/configs/pg.go
@@ -0,0 +1,30 @@
+package configs
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func InitDB() (*pgxpool.Pool, error) {
+	dbUser := os.Getenv("DBUSER")
+	dbPass := os.Getenv("DBPASS")
+	dbHost := os.Getenv("DBHOST")
+	dbPort := os.Getenv("DBPORT")
+	dbName := os.Getenv("DBNAME")
+
+	// Format: postgres://username:password@host:port/dbname
+	cons := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPass, dbHost, dbPort, dbName)
+	db, err := pgxpool.New(context.Background(), cons)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(context.Background()); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
