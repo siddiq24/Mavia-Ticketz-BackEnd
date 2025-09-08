e1d5023f877a11f389b89f5088ddf6b640fc59de
siddiq24 <siddiqrm24@gmail.com>
2025-09-08 09:25:26 +0700

    feat: add validation token

---
+++ internal/middlewares/verify_token.go
@@ -0,0 +1,30 @@
+package middlewares
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+	"github.com/siddiq24/Tickitz-DB/internal/utils"
+)
+
+func VerifyToken(c *gin.Context) {
+	authHeader := c.GetHeader("Authorization")
+	if authHeader == "" {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "missing token"})
+		return
+	}
+
+	tokenStr := strings.TrimPrefix(authHeader, "Bearer ")
+	claims, err := utils.ValidateJWT(tokenStr)
+	if err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "invalid or expired token"})
+		return
+	}
+
+	// simpan username & role
+	c.Set("user_id", claims.UserID)
+	c.Set("username", claims.Username)
+	c.Set("role", claims.Role)
+	c.Next()
+}
