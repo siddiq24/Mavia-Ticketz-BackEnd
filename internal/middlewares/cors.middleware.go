1528987befc1b0dab3f7d29c85fc4aec341914a1
siddiq24 <siddiqrm24@gmail.com>
2025-09-08 09:28:27 +0700

    feat: add specific allowed cors

---
+++ internal/middlewares/cors.middleware.go
@@ -0,0 +1,19 @@
+package middlewares
+
+import (
+	"time"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func CORSMiddleware() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowOrigins:     []string{"http://127.0.0.1:5500"},
+		AllowMethods:     []string{"GET", "POST", "PATCH", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	})
+}
